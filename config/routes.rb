9b94e0d69ff23962699ae8ba552cea6163f2a7c3
EC2 Default User <ec2-user@ip-172-31-43-226.ap-northeast-1.compute.internal>
2025-01-06 15:34:54 +0000

    Aboutページ

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   get 'users/update'
   get 'users/destroy'
   get 'homes/top'
+  get 'homes/about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
