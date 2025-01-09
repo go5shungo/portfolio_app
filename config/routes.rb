253205b3db9050143b088086aa789dc133e62e4f
EC2 Default User <ec2-user@ip-172-31-43-226.ap-northeast-1.compute.internal>
2025-01-09 14:15:09 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,30 @@
 Rails.application.routes.draw do
-  devise_for :users
-  delete 'post_comments/destroy'
-  post 'post_comments/create'
-  get 'posts/edit'
-  get 'posts/show'
-  get 'posts/update'
-  get 'posts/destroy'
-  get 'posts/new'
-  get 'posts/index'
-  get 'posts/create'
-  get 'users/edit'
-  get 'users/show'
-  get 'users/update'
-  get 'users/destroy'
+
   get 'homes/top'
   get 'homes/about', as: 'about'
+
+  devise_for :users
+  scope module: :public do
+    resources :posts do
+      resources :comments,only: [:create,:destroy]
+    end
+    resources :favorite,only: [:create,:destroy]
+    resources :relationships,only: [:create,:destroy]
+    get  "relationships/followings"  => "relatinships#followings"
+    get  "relationships/followers"  => "relatinships#followers"
+    resources :maps
+    resources :questions
+    resources :answers,except: [:index]
+    resources :pets,except: [:show,:index]
+  end
+  namespace :admin do
+    resources :sessions,only: [:new,:create,:destroy]
+    resources :users,only: [:show,:destroy,:index]
+    resources :maps,only: [:show,:destroy,:index]
+    resources :posts,only: [:show,:destroy,:index]
+    resources :questions,only: [:show,:destroy,:index]
+    resources :answers,only: [:show,:destroy,:index]
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
