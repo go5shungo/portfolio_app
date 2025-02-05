73663010e014af74cfb513e4a52f5f9e8de0c437
EC2 Default User <ec2-user@ip-172-31-43-226.ap-northeast-1.compute.internal>
2025-02-05 06:44:24 +0000

    profile_image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @profile_images = @user.profile_image
     @posts = current_user.posts
+    
   end
   
   def edit
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def get_profile_image(width, height)
+    # プロフィール画像を取得するコードをここに記述
+  end
 end
