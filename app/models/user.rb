fe5eb7df0bf99d0e0f22e4248936bcd166279f26
EC2 Default User <ec2-user@ip-172-31-43-226.ap-northeast-1.compute.internal>
2025-02-10 11:18:50 +0000

    Fix kind2

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,10 @@ class User < ApplicationRecord
       has_many :pets, dependent: :destroy
       has_many :favorites, dependent: :destroy
       has_one_attached :profile_image
-      
+      after_create :create_pets
+
+
+
       def get_profile_image(width, height)
         unless profile_image.attached?
           file_path = Rails.root.join('app/assets/images/no_image.jpg')
