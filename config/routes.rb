94190a936bd619abe9a9c4dedf33861a05803401
EC2 Default User <ec2-user@ip-172-31-43-226.ap-northeast-1.compute.internal>
2025-02-19 08:28:07 +0000

    Add admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy] 
+  end
+
   root to: 'homes#top'
   get 'homes/about', as: 'about'
 
